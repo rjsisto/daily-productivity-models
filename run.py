9f654ffbab40883fb0d1cdfeabcf117a9c37513a
Rob Sisto <ristosisto@gmail.com>
2023-08-03 16:49:55 -0500

    modified the run.py so it will be able to create the environment as well

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-#TODO add a cli arguemnt (or automatically detect) if the script is being run in windows or linux/mac
+#maybe switch this over to invoke or something like that
 import argparse
 import os
 
+#TODO create a windows script that emulates the setup_env.sh one
 #code to determine if os is windows or max/linux
 activate = None
 if(os.name == "posix"):
@@ -10,10 +11,11 @@ elif(os.name == "nt"):
     activate = "activate windows python environment"
 
 #TODO come up with a name for this run.py
-parser = argparse.ArgumentParser(prog="run.py", description="Run the various notebooks and the dashboard from here")
+parser = argparse.ArgumentParser(prog="makefile", description="The main way to access the project")
 
 parser.add_argument("-j", "--jup", help="Run the Jupyter Lab Server", action="store_true")
 parser.add_argument("-d", "--dash", help="Run the dashboard", action="store_true")
+parser.add_argument("-c", "--create_env", help="Create the virtual environment", action="store_true")
 
 args = parser.parse_args()
 if(args.jup):
@@ -21,6 +23,8 @@ if(args.jup):
 if(args.dash):
     os.system(activate + "python data_app/model_page.py")
 
+#TODO change this for windows
+if(args.create_env):
+    os.system("bash setup_env.sh")
 
-
-
+parser.print_help()
